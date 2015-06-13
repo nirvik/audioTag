51452dd2c1521fb9b3f74379fd077a62e41efcd5
Nirvik Ghosh <nirvik@localhost.localdomain>
2015-06-14 03:10:31 +0800

    Made a fingerPrinter class...feed the file and return the song ids

--- autotag/fingerprint.py
+++ autotag/fingerprint.py
@@ -1,13 +1,28 @@
 import acoustid 
 import musicbrainzngs 
-import subprocess as sbp
+import subprocess 
 import sys
 
-FILE  = sys.argv[1] 
-p = sbp.Popen(['fpcalc',FILE],stdout=sbp.PIPE, stderr=sbp.PIPE)
-output,err = p.communicate()
-print output
-#Get the fingerprint 
-#Use the acoustid web service to get enter the fingerprint 
-#The webservice will query the fingerprint in the musicbrainz database 
-#acoustid.lookup('S0xa1BKE',fingerprint,duration)
+class FingerPrinter(object):
+
+    def __init__(self,FILE):
+        self.fingerprint = None
+        self.duration = None
+        fpprocess = subprocess.Popen(['fpcalc',FILE],stdout=subprocess.PIPE, stderr = subprocess.PIPE)
+        answer , error  = fpprocess.communicate()
+        length , score = answer.split('\n')[1] , answer.split('\n')[-2]
+        self.fingerprint,self.duration = score[score.find('=')+1:],length[length.find('=')+1:]
+    
+
+if __name__ == '__main__':    
+    FILE  = sys.argv[1]
+    finger = FingerPrinter(FILE)
+    json_acoustid_result  =  acoustid.lookup('S0xa1BKE',finger.fingerprint,finger.duration)
+    for i in json_acoustid_result['results']:
+    	print i.keys()
+
+'''
+Get the fingerprint 
+Use the acoustid web service to get enter the fingerprint 
+The webservice will query the fingerprint in the musicbrainz database 
+'''
