ad50c669adff58de719fd8dfc71e661202989adf
nirvik1993 <nirvik1993@gmail.com>
2015-07-09 03:57:52 +0800

    Whole bunch of changes

--- autotag/main.py
+++ autotag/main.py
@@ -1,7 +1,7 @@
-import musicbrainzngs
+#import musicbrainzngs
 from  fingerprint import * 
 import sys
-import musicbrainz
+from  musicbrainz import MusicUtils
 import operator
 from datetime import datetime
 import tag
@@ -17,69 +17,28 @@ if __name__=='__main__':
 	metadata = {} 
         try:
     	    finger = FingerPrinter(FILE)
-            query = AcoustQuery(finger.fingerprint , finger.duration)
+	    query = MusicUtils.feedfingerprint(finger.fingerprint,finger.duration)
             query.parse_result()
 	except : 
 	    print ' Invalid Song '
 	    continue
-
-    	best_acoustid,score = query.bestacoustid() # get the acoustid that has the maximum score
-
+    	best_acoustid,score = query.bestacoustid() # gets the acoustid that has the maximum score
    	try:
-	    mbids,details = musicbrainz.get_recording_by_id(query.recording_ids[best_acoustid])
+	    mbids,details = query.recording_details(query.recording_ids[best_acoustid])
 	except:
-            print 'seriously ! this shit happend '
+            print 'Something unusual happened ...'
 	    continue
-    	mbid_rec = details
-    	mbid_dates = {} 
-    	temp = {} 
-    	for key,value in details.iteritems():
-            for j in value:
-	    	try:
-	            mbid_dates[j['id']] = datetime.strptime(j['date'],'%Y-%M-%d')
-	    	except:
-		    try:
-		    	mbid_dates[j['id']] = datetime.strptime(j['date'],'%Y-%M')
-		    except:
-			try:
-		    	    mbid_dates[j['id']] = datetime.strptime(j['date'],'%Y')
-			except:
-       		            pass
-	    	temp[j['id']] = key
 
-        # Let's say if date is not there 
-	try:
-    	    coverart_mbid,date = max(mbid_dates.iteritems(),key=operator.itemgetter(1)) # Get the latest cover track 
-	except:
-	    coverart_mbid = random.choice(temp.keys())
-	    date = '2015'
-            
-    	print 'The cover art mbid is {0}'.format(coverart_mbid)
-    	print 'This was released in {0}'.format(date)
-
-        artist = query.artist
+	coverart_mbid , date = query.latestmbid(details)
+	artist = query.artist
 	song = query.song_title
-	
-    	print 'Song : {0}'.format(song)
-        print 'Artist: {0}'.format(artist)
-	
-	key = ''
-	for i,j in mbids.iteritems():
-    	    for k in j:
-	    	if coverart_mbid in j:
-	    	    key = i
-		    break
-    	
+	duration = query.duration
+	album_name = query.extract_album_name(coverart_mbid)
+
 	metadata['title'] = unicode(song)
-	try:
-	    metadata['album'] = unicode(details[key][0]['title'])
-	except:
-	    metadata['album']= unicode(song)
 	metadata['artist'] = unicode(artist)
 	metadata['date'] = unicode(date)
-	metadata['album-art-mbid'] = unicode(coverart_mbid) 
-	try:
-	    #tag.tagit(FILE , metadata)
-	    tag.trialtag(FILE , metadata)
-	except:
-	    pass
+	metadata['album-art-mbid'] = unicode(coverart_mbid)
+	metadata['album'] = album_name
+	metadata['duration'] = duration
+	print metadata
--- autotag/musicbrainz.py
+++ autotag/musicbrainz.py
@@ -1,49 +1,236 @@
+import acoustid
 import musicbrainzngs
-import fingerprint
+from  fingerprint import *
+import datetime
+import random
+import operator 
+import requests 
+import urllib 
 
-musicbrainzngs.auth('','')  # username  , password
-musicbrainzngs.set_useragent('Auto Tagger', '0.1', 'nirvik1993@gmail.com')
+class MusicUtilsException(Exception):
+    def __init__(self,code,msg):
+        self.msg = msg 
+	self.code = code 
 
+    def __str__(self):
+    	return '[Error :{0}] => {1}'.format(self.code , self.msg)
 
-def show_release_details(rel):
-    """Print some details about a release dictionary to stdout.
-    """
-    # "artist-credit-phrase" is a flat string of the credited artists
-    # joined with " + " or whatever is given by the server.
-    # You can also work with the "artist-credit" list manually.
-    try:
-        print("{}".format(str(rel['title'])))
-    except:
-        pass
-    if 'date' in rel:
-	try:
-            print("Released {} ({})".format(rel['date'], rel['status']))
-        except:
-	    pass
-	print("MusicBrainz ID: {}".format(rel['id']))
-
-
-def get_recording_by_id(recording_ids):
+class MusicUtils(object):
     
-    # We need to store more information . As of now , storing only release id to get the album art in future 
-    mbids = {} 
-    song_details = {} 
-    for rid in recording_ids:
-        release_ids = [] 
-        try:
-	    data  = musicbrainzngs.get_recording_by_id(rid,includes=['releases'])['recording']
-	    for i in data['release-list']:
-		try:
-                    if i['status'] == 'Official':
-                        release_ids.append(i['id'])
-		except:
-		    pass
-            mbids[rid] = release_ids  # we use these release ids to get the album art 
-  	    song_details[rid] = data['release-list']
-    	except musicbrainzngs.WebServiceError as exc:
-            print("Something went wrong with the request: %s" % exc)
-	    raise fingerprint.FingerPrinterException('Something wrong with the request. ',3)
-    return (mbids,song_details) 
+    musicbrainzngs.auth('','')  # username  , password
+    musicbrainzngs.set_useragent('Auto Tagger', '0.1', 'nirvik1993@gmail.com')
+    __doc__ = 'A bunch of utilities provided to extract information from the musicbrainzngs'
+    __version__ = '0.0.1'
+    imageurl = 'http://coverartarchive.org/release/'
+    API_KEY = 'S0xa1BKE' 
 
-def get_album_art(release_id):
-    result = get_recording_by_id(release_id)
+    def __init__(self , fingerprint = None , duration = None ):
+        
+	self._mbid = None 
+        self._mbid_dates = {}
+        self._mbids = {} # Recording id to release id's 
+        self._song_details = {}
+        self._backup_date = {}
+	self._fingerprint = fingerprint 
+	self.duration = duration
+	self.song_title =None
+	self.artist = None
+	self.album = None
+	self.date = None
+	# We dont want this to have public access 
+	self._scores = {} 
+	self._recording_ids = {} 
+
+    @classmethod
+    def feedfingerprint(cls,fingerprint,duration):
+        return cls(fingerprint , duration)
+
+    
+    @property
+    def mbids(self):
+    	return self._mbids
+
+    @property
+    def song_details(self):
+        return self._song_details
+
+    @property
+    def mbid(self):
+        return self._mbid 
+
+	
+    @property
+    def recording_ids(self):
+        return self._recording_ids
+    
+    @property
+    def mbid_dates(self):
+        '''
+        Say you want to view the original date of the respective mbid 
+        key : mbid 
+        value : release date 
+        '''
+        return self._mbid_dates
+	
+
+    @mbid_dates.setter
+    def mbid_dates(self,val):
+        '''
+        Having the setter properties , so that you dont mess much with the mbid_dates 
+        '''
+        if isinstance(val,dict) :
+            self._mbid_dates = val
+            
+        
+    def extractdates(self,details):
+        '''
+        Extract all the dates.
+        Basically populates the mbid_dates with the apporpriate data , i.e mbid => date 
+        '''
+        for key,value in details.iteritems():
+            for j in value:
+                try:
+                    self.mbid_dates[j['id']] = datetime.strptime(j['date'],'%Y-%M-%d')
+                except:
+                    try:
+                        self.mbid_dates[j['id']] = datetime.strptime(j['date'],'%Y-%M')
+                    except:
+                        try:
+                            self.mbid_dates[j['id']] = datetime.strptime(j['date'],'%Y')
+                        except:
+                            pass
+                self._backup_date[j['id']] = key
+
+                
+    def latestmbid(self,details):
+        ''' 
+        Gets the information about the latest track of the song and returns the song's mbid 
+        '''
+        self.extractdates(details)
+        try:
+            self._mbid , self.date =  max(self._mbid_dates.iteritems(),key = operator.itemgetter(1))
+        except:
+            self._mbid ,self.date =  (random.choice(self._backup_date.keys()),'2015')
+	return (self._mbid , self.date) 
+        
+
+    def extract_album_name(self,coverart_mbid):
+        ''' Get the album name  ''' 
+        key = ''
+        for i,j in self._mbids.iteritems():
+            for k in j :
+                if coverart_mbid in j:
+                    key = i
+                    break
+        try:
+            self.album =  self._song_details[key][0]['title'].encode('utf-8')
+        except:
+            self.album = ''
+        return self.album
+        
+
+    def recording_details(self,recording_ids):
+        ''' 
+    	We need to store more information . As of now , storing only release id to get the album art in future
+    	'''
+        for rid in recording_ids:
+            release_ids = [] 
+            try:
+	        data  = musicbrainzngs.get_recording_by_id(rid,includes=['releases'])['recording']
+	        for i in data['release-list']:
+		    try:
+                        if i['status'] == 'Official':
+                            release_ids.append(i['id'])
+		    except:
+		        pass
+                    self._mbids[rid] = release_ids
+  	            self._song_details[rid] = data['release-list']
+    	    except musicbrainzngs.WebServiceError as exc:
+                print("Something went wrong with the request: %s" % exc)
+	        raise FingerPrinterException('Something wrong with the request. ',3)
+        return (self._mbids,self._song_details) 
+
+    @staticmethod
+    def albumart(art_mbid):
+        cover_art_json = requests.get(imageurl+art_mbid).text
+        try:
+	    response = json.loads(cover_art_json)
+	except:
+	    location , header = '' , {'content-type':'failed','content-length':0}
+	    return (location , header )
+	try:
+	    picture = response['images'][0]['image']
+	    print picture
+	    location , header = urllib.urlretrieve(picture,"filename.jpeg")
+	except :
+	    print response
+	    print cover_art_json
+	    print 'There is no album art for this file '
+	    location = ''
+	    header = {'content-type':'failed','content-length':0}
+	print ' Image located in {0} and downloaded {1} '.format(location,header['content-length'])
+	if header['content-type'] == 'image/jpg':
+	    header['content-type'] = 'image/jpeg'
+	if header['content-type'] != 'failed':
+	    return (location , header) 
+	else :
+	    raise MusicUtilsException('1','Image not found') 
+ 
+    def bestacoustid(self):
+        return max(self._scores.iteritems(),key=operator.itemgetter(1))
+    
+    def parse_result(self):
+        acoustids = []
+        json_file = acoustid.lookup(MusicUtils.API_KEY , self._fingerprint , self.duration) 
+	temp_song  = {}
+        temp_artist = {}
+	if json_file['status'] != u'ok' or len(json_file['results'])==0:
+	    raise MusicUtilException('2','Bad look up, ended with a bad status report')
+	for result in json_file['results']:
+            acoustids.append(result['id'])
+            self._scores[result['id']] = result['score']
+            if 'recordings' in result:
+                rids = [] # Recording Ids 
+                for recording in result['recordings']:
+                    rids.append(recording['id'])
+		    try:
+	  	        if recording['title'] in temp_song:
+		            temp_song[recording['title']] += 1
+		        else:
+		    	    temp_song[recording['title']] = 0 
+		    except:
+			pass	
+		    try:
+		    	name = '' 
+			ok = False
+                        for person in recording['artists']:
+			    if 'joinphrase' in person:
+			    	name +=  person['name'] +person['joinphrase']
+				ok = True
+				continue
+			    			    
+			    #adding the last artist 
+			    if ok:
+			        name = name + ' ' + person['name']
+			    else:
+				name = person['name']
+                            if name in temp_artist :
+                                temp_artist[name] +=1
+                            else:
+                                temp_artist[name] = 0
+		    except:
+			pass
+            self.recording_ids[result['id']] = rids
+	self.song_title , self.artist =  max(temp_song.iteritems(), key = operator.itemgetter(1))[0].encode('utf-8') , max(temp_artist.iteritems(), key = operator.itemgetter(1))[0].encode('utf-8')
+
+if __name__ == '__main__':
+    import sys
+    music = sys.argv[1]
+    finger = FingerPrinter(music)
+    query = MusicUtils.feedfingerprint(finger.fingerprint , finger.duration)
+    query.parse_result()
+    best_acoust , score = query.bestacoustid()
+    mbids , details = query.recording_details(query.recording_ids[best_acoust])
+    coverart_mbid , date = query.latestmbid(details)
+    album_name = query.extract_album_name(coverart_mbid)
+    print ' Artist :{0} \n Album  :{1} \n Song   :{2} \n Date   :{3} \n Duration:{4} \n mbid   :{5}'.format(query.artist,query.album,query.song_title,query.date,query.duration,query.mbid)
